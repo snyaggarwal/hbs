e3d247864262e7217aaf625d32842e589fead653
Sny <sny.aggarwal@gmail.com>
2018-01-03 14:51:33 +0530

    added formspree

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -194,33 +194,33 @@ $(document).ready(function() {
     $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
     	$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
     });
-	$('.contact-form form').submit(function(e) {
-		e.preventDefault();
-	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
-	    var postdata = $('.contact-form form').serialize();
-	    $.ajax({
-	        type: 'POST',
-	        url: 'assets/contact.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-	            if(json.emailMessage != '') {
-	                $('.contact-form form .contact-email').addClass('contact-error');
-	            }
-	            if(json.subjectMessage != '') {
-	                $('.contact-form form .contact-subject').addClass('contact-error');
-	            }
-	            if(json.messageMessage != '') {
-	                $('.contact-form form textarea').addClass('contact-error');
-	            }
-	            if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-	                $('.contact-form form').fadeOut('fast', function() {
-	                    $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
-	                });
-	            }
-	        }
-	    });
-	});
+	// $('.contact-form form').submit(function(e) {
+	// 	e.preventDefault();
+	//     $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
+	//     var postdata = $('.contact-form form').serialize();
+	//     $.ajax({
+	//         type: 'POST',
+	//         url: 'assets/contact.php',
+	//         data: postdata,
+	//         dataType: 'json',
+	//         success: function(json) {
+	//             if(json.emailMessage != '') {
+	//                 $('.contact-form form .contact-email').addClass('contact-error');
+	//             }
+	//             if(json.subjectMessage != '') {
+	//                 $('.contact-form form .contact-subject').addClass('contact-error');
+	//             }
+	//             if(json.messageMessage != '') {
+	//                 $('.contact-form form textarea').addClass('contact-error');
+	//             }
+	//             if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+	//                 $('.contact-form form').fadeOut('fast', function() {
+	//                     $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
+	//                 });
+	//             }
+	//         }
+	//     });
+	// });
 
     
 });
